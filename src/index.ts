093efb9761dde215bf0cb323b90da0880153addd
Pratik Raut <pratikraut0000@gmail.com>
2021-08-21 15:57:54 +0530

    Question Routes Completed and Basic Setup of Answer Routes Done

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import loginRouter from "./LoginRoutes/loginRoute";
 import questionRouter from "./QuestionRoutes/QuestionRoute";
+import answerRouter from "./AnswerRoutes/AnswerRoute";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 // MIDDLEWARES HERE...
 app.use("/login", loginRouter);
 app.use("/questions", questionRouter);
+app.use("/answers", answerRouter);
 
 ///////////////////////
 // LISTENING STARTS HERE...
